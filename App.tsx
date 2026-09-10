c5b007bf2f57be3375e944e0e84554c5e71efcde
WeslleyMB <2003weslleymb@gmail.com>
2026-09-10 16:29:51 -0300

    style: mudança do background das categorias do cardapio

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // 1. Altere a importação para puxar a HomeScreen
-import HomeScreen from './src/screens/Home'; 
+import HomeScreen from './src/view/homeView'; 
 
 export default function App() {
   // 2. Substitua o retorno para renderizar a nova tela
